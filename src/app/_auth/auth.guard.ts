e7b1961b0e2259c5c6f0a6c2feb309189aa9589c
Awais Arif <arifawais328@gmail.com>
2026-05-26 17:21:05 +0500

    upgrade Angular and related dependencies to version 16.x

--- src/app/_auth/auth.guard.ts
+++ src/app/_auth/auth.guard.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { CanActivate, Router } from '@angular/router';
+import { Router } from '@angular/router';
 import { NbAuthService } from '@nebular/auth';
 
 import { tap } from "rxjs/operators";
@@ -7,7 +7,7 @@ import { tap } from "rxjs/operators";
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private atuhService: NbAuthService, private router: Router){}
   canActivate(){
     return this.atuhService.isAuthenticated()
