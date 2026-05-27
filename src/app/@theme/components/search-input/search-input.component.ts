0f42775cdbb7f341b183bd35465e2184290aa22d
Awais Arif <arifawais328@gmail.com>
2026-05-27 09:42:50 +0500

    updated angular to v21

--- src/app/@theme/components/search-input/search-input.component.ts
+++ src/app/@theme/components/search-input/search-input.component.ts
@@ -1,9 +1,9 @@
 import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular/core';
 
 @Component({
-    selector: 'ngx-search-input',
-    styleUrls: ['./search-input.component.scss'],
-    template: `
+  selector: 'ngx-search-input',
+  styleUrls: ['./search-input.component.scss'],
+  template: `
     <i class="control-icon ion ion-ios-search"
        (click)="showInput()"></i>
     <input placeholder="Type your search request here..."
@@ -12,18 +12,21 @@ import { Component, ElementRef, EventEmitter, Output, ViewChild } from '@angular
            (blur)="hideInput()"
            (input)="onInput($event)">
   `,
-    standalone: false
+  standalone: false
 })
 export class SearchInputComponent {
-  @ViewChild('input', { static: true }) input: ElementRef;
+  @ViewChild('input', { static: true }) input: ElementRef | undefined;
 
+  // eslint-disable-next-line @angular-eslint/no-output-native
   @Output() search: EventEmitter<string> = new EventEmitter<string>();
 
   isInputShown = false;
 
   showInput() {
     this.isInputShown = true;
-    this.input.nativeElement.focus();
+    if (this.input) {
+      this.input.nativeElement.focus();
+    }
   }
 
   hideInput() {
